1b0465618a81721c14bc2fefa887860d3cc74172
Christian Hackl <hackl.chris@googlemail.com>
2022-04-04 13:18:05 +0200

    [BUGFIX] position of psr-middleware

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -5,7 +5,9 @@ return [
         'hauerheinrich/typo3monitorapi' => [
             'target' => \HauerHeinrich\Typo3MonitorApi\Middleware\MonitorApi::class,
             'before' => [
-                'typo3/cms-redirects/redirecthandler'
+                'typo3/cms-redirects/redirecthandler',
+                'typo3/cms-frontend/base-redirect-resolver',
+                'typo3/cms-frontend/static-route-resolver'
             ],
             'after' => [
                 'typo3/cms-frontend/authentication'
