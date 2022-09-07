b9f571205439a7a2a1f3b74b86bee10d865be391
Christian Hackl <hackl.chris@googlemail.com>
2022-09-07 17:32:57 +0200

    [TASK] set versions number to 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,18 +1,17 @@
 <?php
 $EM_CONF['typo3_monitor_api'] = [
-    'title' => 'Hauer-Heinrich - ',
+    'title' => 'Hauer-Heinrich - TYPO3 monitor api - allows to retrieve various information about the installed TYPO3 cms (response: json).',
     'description' => 'Hauer-Heinrich',
     'category' => 'be',
     'author' => 'Christian Hackl',
-    'author_email' => 'chackl@hauer-heinrich.de',
+    'author_email' => 'info@hauer-heinrich.de',
     'author_company' => 'Hauer-Heinrich.de',
-    'state' => 'stable',
+    'state' => 'beta',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-11.5.99',
-            'dashboard' => ''
+            'typo3' => '10.4.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => []
