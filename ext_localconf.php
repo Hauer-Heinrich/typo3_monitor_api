2738c1abb1c6710b0fabcc145ce47b98a37a1870
Christian Hackl <hackl.chris@googlemail.com>
2022-12-06 11:48:12 +0100

    [TASK] comment out wrong not used include of typoscript

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,12 +4,7 @@ if (!defined('TYPO3_MODE')) {
 }
 
 call_user_func(static function () {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
-        "@import 'EXT:typo3_monitor_api/Configuration/TypoScript/setup.typoscript'"
-    );
-
-    // Add UserTS config as default for all BE users //
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
-        "@import 'EXT:typo3_monitor_api/Configuration/TsConfig/User/0100_default.typoscript'"
-    );
+    // \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+    //     "@import 'EXT:typo3_monitor_api/Configuration/TypoScript/setup.typoscript'"
+    // );
 });
